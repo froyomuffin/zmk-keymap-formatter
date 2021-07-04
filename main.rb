26e2b607e71bf141885ecbc7ffe21822902f2eea
Thomas Wang <tw@eclair.io>
2021-07-04 23:26:32 +0800

    Add KeymapFormatter to format entire keymap files

--- main.rb
+++ main.rb
@@ -2,9 +2,17 @@ require 'rubygems'
 require 'bundler/setup'
 Dir["app/**/*.rb"].each { |file| require_relative file }
 
-SAMPLE_LAYER = "samples/layer.keymap"
+FILE = "samples/nice60.keymap"
 
-puts "Starting"
+formatter = KeymapFormatter.new(keymap_file: FILE)
+
+result = formatter.format
+
+result.each { |e| puts e }
+
+exit
+
+SAMPLE_LAYER = "samples/layer.keymap"
 
 key_lines = File.readlines(SAMPLE_LAYER)
 kb = Keyboard.new(
